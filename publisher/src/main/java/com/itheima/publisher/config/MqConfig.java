fe80bb1c1a6bf9108ef8475c1478366431c8b64c
DESKTOP-NUIDTQL\Dell <2740566382@qq.com>
2025-11-04 11:09:12 +0800

    可靠性

---
+++ publisher/src/main/java/com/itheima/publisher/config/MqConfig.java
@@ -0,0 +1,32 @@
+package com.itheima.publisher.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ReturnedMessage;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class MqConfig {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @PostConstruct
+    public void init(){
+        rabbitTemplate.setReturnsCallback(new RabbitTemplate.ReturnsCallback() {
+            @Override
+            public void returnedMessage(ReturnedMessage returnedMessage) {
+                log.error("监听到了消息return callback");
+                log.debug("exchange: {}",returnedMessage.getExchange());
+                log.debug("routingKey: {}",returnedMessage.getRoutingKey());
+                log.debug("message: {}",returnedMessage.getMessage());
+                log.debug("replyCode: {}",returnedMessage.getReplyCode());
+                log.debug("replyText: {}",returnedMessage.getReplyText());
+            }
+        });
+    }
+}
--- publisher/src/test/java/com/itheima/publisher/SpringAmqpTest.java
+++ publisher/src/test/java/com/itheima/publisher/SpringAmqpTest.java
@@ -1,16 +1,21 @@
 package com.itheima.publisher;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 @SpringBootTest
 class SpringAmqpTest {
 
@@ -54,5 +59,34 @@ class SpringAmqpTest {
         map.put("age",21);
         //3.发送消息
         rabbitTemplate.convertAndSend("object.queue",map);
+    }
+
+    @Test
+    public void testConfirmCallback() throws InterruptedException {
+        //0.创建CorrelationData
+        CorrelationData cd = new CorrelationData(UUID.randomUUID().toString());
+        cd.getFuture().addCallback(new ListenableFutureCallback<CorrelationData.Confirm>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("spring amqp 处理确认结果异常",ex);
+            }
+
+            @Override
+            public void onSuccess(CorrelationData.Confirm result) {
+                if (result.isAck()){
+                    log.debug("收到ConfirmCallback ack，消息发送成功！");
+                }else {
+                    log.error("收到ConfirmCallback nack，消息发送失败！reason: {}",result.getReason());
+                }
+            }
+        });
+        //1.交换机名
+        String exchangeName = "hmall.direct";
+        //2.消息
+        String msg = "blue message!";
+        //3.发送消息
+        rabbitTemplate.convertAndSend(exchangeName,"blue2",msg,cd);
+
+        Thread.sleep(2000);
     }
 }
